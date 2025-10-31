71b2944dd1e631d6461c109ae95fda600889bccb
rraaull7 <rraaulldemiguel@gmail.com>
2025-10-31 14:11:57 +0100

    Update SumaMientrasEsPositivo.java

--- SumaMientrasEsPositivo.java
+++ SumaMientrasEsPositivo.java
@@ -1,3 +1,21 @@
-public class SumaMientrasEsPositivo {
+import java.util.Scanner;
 
+public class SumaMientrasEsPositivo {
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int numero;
+int suma = 0;
+
+do {
+System.out.print("Introduce un número: ");
+numero = sc.nextInt();
+
+if (numero >= 0) {
+suma += numero;
+}
+} while (numero >= 0); 
+
+System.out.println("La suma es: " + suma);
+sc.close();
+    }
 }
